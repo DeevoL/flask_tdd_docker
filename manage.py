a0e42821fe742041c3c2d0e4edc61131e6ef1d02
deevol <leung.devin@yahoo.com>
2020-06-11 19:55:59 -0700

    finished part1/ routes up and tested

--- manage.py
+++ manage.py
@@ -1,7 +1,27 @@
-from flask.cli import FlaskGroup
-from project import app
+import sys
 
-cli = FlaskGroup(app)
+from flask.cli import FlaskGroup
+
+from project import create_app, db
+from project.api.models import User
+
+app = create_app()
+cli = FlaskGroup(create_app=create_app)
+
+
+
+#registers command 'recreate_db' to the CLI so that we can run it from the command line
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+@cli.command('seed_db')
+def seed_db():
+    db.session.add(User(username='michael', email="hermanmu@gmail.com"))
+    db.session.add(User(username='michaelherman', email="michael@mherman.org"))
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
